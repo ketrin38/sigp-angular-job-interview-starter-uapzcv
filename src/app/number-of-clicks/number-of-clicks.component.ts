1e0004383234143151a895f218ea0950bd72bf5b
ketrin38 <ketrin.pilarcik@gmail.com>
2019-11-01 09:05:47 +0100

    3 question

--- src/app/number-of-clicks/number-of-clicks.component.ts
+++ src/app/number-of-clicks/number-of-clicks.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-number-of-clicks',
   templateUrl: './number-of-clicks.component.html',
   styleUrls: ['./number-of-clicks.component.css'],
-  encapsulation: ViewEncapsulation.None
 })
 export class NumberOfClicksComponent implements OnInit {
   @Input() numberOfClicks: number;
